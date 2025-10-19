c19fb64a2a381f71c6bb29b8069c851de2fe8fde
Alberto838 <albert34321@gmail.com>
2025-10-19 22:47:03 +0200

    removed unnecessary lines

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,35 +3,26 @@ import { test, expect } from '@playwright/test'
 test.describe('User login to Demobank', () => {
 	test('successful login with correct credentials', async ({ page }) => {
 		await page.goto('https://demo-bank.vercel.app/')
-		await page.getByTestId('login-input').click()
 		await page.getByTestId('login-input').fill('test1234')
-		await page.getByTestId('password-input').click()
 		await page.getByTestId('password-input').fill('12345567')
 		await page.getByTestId('login-button').click()
-		await page.getByTestId('user-name').click()
-		await page.getByRole('link', { name: 'płatności' }).click()
 
 		await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy')
 	})
 
 	test('unsuccessful login with incorrect username', async ({ page }) => {
 		await page.goto('https://demo-bank.vercel.app/')
-		await page.getByTestId('login-input').click()
 		await page.getByTestId('login-input').fill('test')
-		await page.getByTestId('password-input').click()
 		await page.getByTestId('password-input').fill('12345')
-		await page.getByTestId('error-login-id').click()
 
 		await expect(page.getByTestId('error-login-id')).toHaveText(
 			'identyfikator ma min. 8 znaków'
 		)
 	})
 
-	test.only('unsuccessful login with too short password', async ({ page }) => {
+	test('unsuccessful login with too short password', async ({ page }) => {
 		await page.goto('https://demo-bank.vercel.app/')
-		await page.getByTestId('login-input').click()
 		await page.getByTestId('login-input').fill('tester12')
-		await page.getByTestId('password-input').click()
 		await page.getByTestId('password-input').fill('12345')
 		await page.getByTestId('password-input').blur()
 
