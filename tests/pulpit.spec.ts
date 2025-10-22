015b25a07431bb7faebe3632461f0cd50ff7c179
Alberto838 <albert34321@gmail.com>
2025-10-22 19:34:27 +0200

    analyzed flaky tests

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,11 +1,14 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Pulpit tests', () => {
+	test.describe.configure({ retries: 3 })
 	test.only('quick payment with correct data', async ({ page }) => {
 		await page.goto('https://demo-bank.vercel.app/')
 		await page.getByTestId('login-input').fill('test1234')
 		await page.getByTestId('password-input').fill('12345567')
 		await page.getByTestId('login-button').click()
+
+		await page.waitForLoadState('domcontentloaded')
 
 		await page.locator('#widget_1_transfer_receiver').selectOption('2')
 		await page.locator('#widget_1_transfer_amount').fill('200')
