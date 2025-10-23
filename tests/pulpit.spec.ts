46af3a1974425176fc532e4290ad13ccc74acfc0
Alberto838 <albert34321@gmail.com>
2025-10-23 20:21:21 +0200

    added mobile top-up test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -21,4 +21,20 @@ test.describe('Pulpit tests', () => {
 			'Przelew wykonany! Chuck Demobankowy - 200,00PLN - Podróż'
 		)
 	})
+
+	test('successful mobile top-up', async ({ page }) => {
+		await page.goto('https://demo-bank.vercel.app/')
+		await page.getByTestId('login-input').fill('testerlo')
+		await page.getByTestId('password-input').fill('password')
+		await page.getByTestId('login-button').click()
+		await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx')
+		await page.locator('#widget_1_topup_amount').fill('100')
+		await page.locator('#uniform-widget_1_topup_agreement > span').click()
+		await page.getByRole('button', { name: 'doładuj telefon' }).click()
+		await page.getByTestId('close-button').click()
+
+		await expect(page.getByTestId('message-text')).toHaveText(
+			'Doładowanie wykonane! 100,00PLN na numer 502 xxx xxx'
+		)
+	})
 })
