0c8664e492465fc80f7a972dd649ca4ce7af4cdb
Alberto838 <albert34321@gmail.com>
2025-10-24 12:08:36 +0200

    first AAA implementation

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,22 +3,33 @@ import { test, expect } from '@playwright/test'
 test.describe('Pulpit tests', () => {
 	test.describe.configure({ retries: 3 })
 	test('quick payment with correct data', async ({ page }) => {
-		await page.goto('https://demo-bank.vercel.app/')
-		await page.getByTestId('login-input').fill('test1234')
-		await page.getByTestId('password-input').fill('12345567')
+		// Arrange
+		const url = 'https://demo-bank.vercel.app/'
+		const userId = 'test1234'
+		const userPassword = '12345567'
+		const receiverId = '2'
+		const transferAmount = '200'
+		const transferTitle = 'Podróż'
+		const expectedReceiver = 'Chuck Demobankowy'
+
+		// Act
+		await page.goto(url)
+		await page.getByTestId('login-input').fill(userId)
+		await page.getByTestId('password-input').fill(userPassword)
 		await page.getByTestId('login-button').click()
 
 		await page.waitForLoadState('domcontentloaded')
 
-		await page.locator('#widget_1_transfer_receiver').selectOption('2')
-		await page.locator('#widget_1_transfer_amount').fill('200')
-		await page.locator('#widget_1_transfer_title').fill('Podróż')
+		await page.locator('#widget_1_transfer_receiver').selectOption(receiverId)
+		await page.locator('#widget_1_transfer_amount').fill(transferAmount)
+		await page.locator('#widget_1_transfer_title').fill(transferTitle)
 
 		await page.getByRole('button', { name: 'wykonaj' }).click()
 		await page.getByTestId('close-button').click()
 
+		// Assert
 		await expect(page.getByTestId('message-text')).toHaveText(
-			'Przelew wykonany! Chuck Demobankowy - 200,00PLN - Podróż'
+			`Przelew wykonany! ${expectedReceiver} - ${transferAmount},00PLN - ${transferTitle}`
 		)
 	})
 
